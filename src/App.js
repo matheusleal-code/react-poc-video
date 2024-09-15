2fb3feb984eaab6bcb09ce05cc6892292e7ff81c
Matheus Leal <lealmatheus.2001@gmail.com>
2024-09-15 07:58:06 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useRef } from 'react';
 import './App.css';
 import Video from './Video';
 import WebCam from './WebCam';
 import WebcamRecorder from './WebcamRecorder';
 
 function App() {
+  const videoRef = useRef(null);
+
+  const startVideoAndRecording = () => {
+    if (videoRef.current) {
+      videoRef.current.play(); // Inicia a reprodução do vídeo
+    }
+  };
+
   return (
     <div className="App">
-      <Video />
+      <video
+              src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+              controls={false}
+              style={{ width: "30%"}}
+              ref={videoRef}
+            ></video>
+      <WebcamRecorder />
+      <button onClick={startVideoAndRecording}>Iniciar video</button>
     </div>
   );
 }
