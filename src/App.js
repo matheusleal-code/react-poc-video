61e52778cfcf37bf7a5e63d69788f886ab145ff8
Matheus Leal <lealmatheus.2001@gmail.com>
2024-09-16 07:59:44 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -16,10 +16,11 @@ function App() {
     <div className="App">
       <video
               src="https://edisciplinas.usp.br/pluginfile.php/5196653/mod_resource/content/1/V%C3%ADdeo.mp4"
-              controls={true}
+              controls={false}
               playsInline
               style={{ width: "50%"}}
               ref={videoRef}
+              poster="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/PNG_Test.png/477px-PNG_Test.png?20240730205008"
             ></video>
       <WebcamRecorder />
       <button onClick={startVideoAndRecording}>Iniciar video</button>
