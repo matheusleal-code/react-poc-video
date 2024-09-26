763b55acd91c685cf7e74d8db66f8b13456b97d5
Matheus Leal <lealmatheus.2001@gmail.com>
2024-09-26 10:20:12 -0300

    teste

--- src/Video.js
+++ src/Video.js
@@ -1,50 +1,45 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./index.css";
-import { v4 as uuid } from "uuid";
-import Webcam from "react-webcam";
 
 const Video = () => {
-  const [videoElement, setVideoElement] = useState(null);
+  const [isVideoReady, setIsVideoReady] = useState(false);
+  const videoRef = useRef(null);
 
   useEffect(() => {
     const preloadResources = async () => {
       try {
         // Pré-carregamento da thumbnail
-        const imgPromise = new Promise((resolve, reject) => {
+        await new Promise((resolve, reject) => {
           const img = new Image();
-          img.onload = () => {
-            resolve();
-          };
+          img.onload = resolve;
           img.onerror = reject;
           img.src = 'https://dummyimage.com/300x200/000/fff';
         });
-        console.log('caiu ');
-        // Pré-carregamento do vídeo
-        const videoPromise = new Promise((resolve, reject) => {
-          const video = document.createElement("video");
-          video.preload = "auto";
-          video.muted = true;
-          video.oncanplaythrough = () => {
-            setVideoElement(video);
-            console.log('video ', video);
-            resolve();
-          };
-          video.onerror = reject;
-          video.src = 'https://seenow-destination.s3.amazonaws.com/videos/ccd1df03-99e7-4269-9e9b-9cd6e763cfb3.mp4';
-        });
-        await Promise.all([imgPromise, videoPromise]);
+
+        setIsVideoReady(true);
       } catch (err) {
-        console.error("Erro ao buscar ou carregar dados do vídeo:", err);
-      } 
+        console.error("Erro ao carregar recursos:", err);
+      }
     };
 
     preloadResources();
   }, []);
 
+  const handleCanPlay = () => {
+    console.log("Vídeo pode ser reproduzido");
+  };
+
   return (
     <div>
-      {videoElement && (
-        <video src="https://seenow-destination.s3.amazonaws.com/videos/ccd1df03-99e7-4269-9e9b-9cd6e763cfb3.mp4" controls={true}></video>
+      {isVideoReady && (
+        <video
+          ref={videoRef}
+          src="https://seenow-destination.s3.amazonaws.com/videos/ccd1df03-99e7-4269-9e9b-9cd6e763cfb3.mp4"
+          controls={true}
+          playsInline
+          preload="auto" // Alterado de "metadata" para "auto"
+          onCanPlay={handleCanPlay}
+        />
       )}
     </div>
   );
